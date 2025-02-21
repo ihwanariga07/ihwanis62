bebad2842aa9e757577bd920c96d46b5a7410499
ihwanariga07 <ihwanariga.ia@gmail.com>
2025-02-21 10:38:55 +0700

    coment semua route

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::fallback (function () {
-    return view('notfound');
-});
+// Route::fallback (function () {
+//     return view('notfound');
+// });
 
 
 /*
@@ -18,19 +18,19 @@ Route::fallback (function () {
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/homepage', function () {
-    return view('halaman1');
-});
+// Route::get('/homepage', function () {
+//     return view('halaman1');
+// });
 
-Route::get('mahasiswa/{nama}',function ($nama){
-    echo "ini halaman Mahasiswa dengan nama $nama";
-});
+// Route::get('mahasiswa/{nama}',function ($nama){
+//     echo "ini halaman Mahasiswa dengan nama $nama";
+// });
 
 
-Route::get('/hello/hello1/hello2/hello3',function (){
-    echo "Hello World";
-});
+// Route::get('/hello/hello1/hello2/hello3',function (){
+//     echo "Hello World";
+// });
